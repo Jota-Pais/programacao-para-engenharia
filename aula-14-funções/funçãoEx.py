31d34c99ade83e8ed92a05e6c61a21a6e3b6b403
Jota-Pais <jotapais07@gmail.com>
2025-06-15 00:49:33 -0300

    aula 14 funções

---
+++ aula-14-funções/funçãoEx.py
@@ -0,0 +1,14 @@
+def function(a, b):
+    a= a+1
+    resultado = a+b
+    return resultado
+soma1 = function(10, 20)
+print(soma1)
+
+
+def functionNone(c,d):
+    result = c + d 
+    print(result)
+
+A = functionNone(12,14)
+print(A)
